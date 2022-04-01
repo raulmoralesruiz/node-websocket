c39064393242b52d5477a69b2faf70f1a5ca5fa5
Raúl Morales <raulmdaw@gmail.com>
2022-04-01 15:04:48 +0200

    FC. Mensajes de conexion y desconexion del cliente

---
+++ public/js/socket-client.js
@@ -0,0 +1,19 @@
+// Referencias del HTML
+const lblOnline = document.querySelector("#lblOnline");
+const lblOffline = document.querySelector("#lblOffline");
+
+const socket = io();
+
+socket.on("connect", () => {
+  console.log("Conectado al servidor");
+
+  lblOffline.style.display = "none";
+  lblOnline.style.display = "";
+});
+
+socket.on("disconnect", () => {
+  console.log("Desconectado del servidor");
+
+  lblOffline.style.display = "";
+  lblOnline.style.display = "none";
+});
